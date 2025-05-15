8e85bb3755d37824d8394c96092d7ba3c8c566b8
Lonzieeee <wanderilorna@gmail.com>
2025-05-15 10:36:11 +0300

    16th

--- src/pages/Technologies.jsx
+++ src/pages/Technologies.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Technologies.css';
 import laptopImage from '../assets/laptop.png'; 
 import plane from '../assets/setPaceWhite.png'
+import blue from '../assets/icon-bend-blue_1@2x.png';
 
 function Technologies() {
   return (
@@ -9,7 +10,7 @@ function Technologies() {
       
       
 
-      {/* Section 2: Technologies with gradient background */}
+      
       <section className="technologies-section">
         <div className="technologies-container">
           <div className="tech-text">
@@ -21,10 +22,48 @@ function Technologies() {
             </p>
           </div>
           <div className="tech-image">
-            <img src={laptopImage} alt="Laptop with technology icons" />
+            <img src={laptopImage} alt="Laptop with  icons" />
           </div>
         </div>
       </section>
+
+<section className="top-technologies">
+  <h1>Our Top Technologies</h1>
+  <div className="top">
+    
+    <div className="four-sections">
+      <div className="internet">
+        <h2>Internet Of Things</h2>
+        <p>Custom software engineering for connected devices, industrial IoT ecosystems, and smart infrastructure solutions.</p>
+      </div>
+       <div className="artificial">
+        <h2>Artificial intelligence</h2>
+        <p>Create systems with integrated AI for reading processing images and videos</p>
+      </div>
+       <div className="reality">
+        <h2>Augmented Reality</h2>
+        <p>Build enviromental-interactive systems that respond to real time changes</p>
+      </div>
+       <div className="crypto">
+        <h2>Cryptography</h2>
+        <p>Encrypt your data with military-grade encryption ciphers</p>
+      </div>
+    </div>
+    <div className="imga">
+      <img src={blue}alt="blue" />
+    </div>
+  </div>
+
+</section>
+
+
+
+
+
+
+
+
+
       <section className="future-proof-section">
         <div className="future-left">
           <h2>
